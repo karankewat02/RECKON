0079405ef4c9bbfc1ad01a5f0f42a5ea7d3b25aa
Prakhar Dubey <prakhardubey2002@gmail.com>
2023-03-18 18:52:29 +0530

    basic config updated

--- pages/create_nft.jsx
+++ pages/create_nft.jsx
@@ -1,9 +1,10 @@
-import React, { useState } from "react";
 import axios from "axios";
+import React, { useState } from "react";
+import { toast } from "react-toastify";
 // import './resources/css/custom2.css';
-import { signAndConfirmTransactionFe } from "./utilityfunc";
 import useWalletBalance from '../context/WalletBalanceProvider';
 import disPic from './resources/images/upload-file.jpg';
+import { signAndConfirmTransactionFe } from "./utilityfunc";
 const xApiKey = "5Bg8vrE-x6zPYo6S"; //Enter Your x-api-key here
 const Create = () => {
 	const { walletAddress } = useWalletBalance();
@@ -15,13 +16,13 @@ const Create = () => {
 	const [name, setName] = useState();
 	const [symbol, setSymbol] = useState();
 	const [desc, setDesc] = useState();
-	const [attr, setAttr] = useState(JSON.stringify([{"trait_type": "edification","value": "100"}]));
+	const [attr, setAttr] = useState(JSON.stringify([{ "trait_type": "edification", "value": "100" }]));
 	const [extUrl, setExtUrl] = useState();
 	const [maxSup, setMaxSup] = useState(0);
 	const [roy, setRoy] = useState(99);
-	const [adres,setAdres]=useState('s');
-	const [minted,setMinted] = useState();
-	const [saveMinted,setSaveMinted] = useState();
+	const [adres, setAdres] = useState('s');
+	const [minted, setMinted] = useState();
+	const [saveMinted, setSaveMinted] = useState();
 	const [errorRoy, setErrorRoy] = useState();
 
 	const [status, setStatus] = useState("Awaiting Upload");
@@ -29,17 +30,17 @@ const Create = () => {
 
 	const [connStatus, setConnStatus] = useState(true);
 
-	const callback = (signature,result) => {
-		console.log("Signature ",signature);
-		console.log("result ",result);
-		if(signature.err === null)
-		{
+	const callback = (signature, result) => {
+		console.log("Signature ", signature);
+		console.log("result ", result);
+		if (signature.err === null) {
 			setMinted(saveMinted);
 			setStatus(`success: Successfully Signed and Minted.  and ${saveMinted} my: ${adres} `);
 		}
-	  }
+	}
 
 	const mintNow = (e) => {
+		toast("Transaction Intialised wait while we connect")
 		e.preventDefault();
 		setStatus("Loading");
 		let formData = new FormData();
@@ -71,16 +72,15 @@ const Create = () => {
 			// Handle the response from backend here
 			.then(async (res) => {
 				console.log(res);
-				if(res.data.success === true)
-				{
+				if (res.data.success === true) {
 					setStatus(`success: Transaction Created. Signing Transactions. Please Wait. ${res.data.result.mint} and ${saveMinted} `);
 					const transaction = res.data.result.encoded_transaction;
 					setSaveMinted(res.data.result.mint);
 					setAdres(res.data.result.mint);
-					const ret_result = await signAndConfirmTransactionFe(network,transaction,callback);
-            		console.log(ret_result);
+					const ret_result = await signAndConfirmTransactionFe(network, transaction, callback);
+					console.log(ret_result);
 					setDispResp(res.data);
-					
+
 				}
 			})
 
@@ -96,7 +96,7 @@ const Create = () => {
 		<div className="gradient-background">
 			<div className="container p-5">
 				{connStatus && (<div className="form-container border border-primary rounded py-3 px-5" style={{ backgroundColor: "#FFFFFFEE" }}>
-					<h3 className="pt-4">Create An Art Nft</h3>
+					<h3 className="pt-4">Create An Nft</h3>
 					<p>
 						Choose the downloaded image in upload/slect file
 					</p>
@@ -134,80 +134,80 @@ const Create = () => {
 						<div className="fields">
 							<table className="table">
 								<tbody>
-								<tr>
-									<td className="py-4 ps-2 w-50 text-start">
+									<tr>
+										<td className="py-4 ps-2 w-50 text-start">
 
-										Network<br />
+											Network<br />
 
-										<small>Solana blockchain environment (testnet/devnet/mainnet-beta)</small>
+											<small>Solana blockchain environment (testnet/devnet/mainnet-beta)</small>
 
-									</td>
-									<td className="px-5 pt-4">
-										<select
-											name="network"
-											className="form-select"
-											onChange={(e) => setnetwork(e.target.value)}
-										>
-											<option value="devnet">Devnet</option>
-											<option value="testnet">Testnet</option>
-											<option value="mainnet-beta">Mainnet Beta</option>
-										</select>
+										</td>
+										<td className="px-5 pt-4">
+											<select
+												name="network"
+												className="form-select"
+												onChange={(e) => setnetwork(e.target.value)}
+											>
+												<option value="devnet">Devnet</option>
+												<option value="testnet">Testnet</option>
+												<option value="mainnet-beta">Mainnet Beta</option>
+											</select>
 
-									</td>
-								</tr>
-								<tr>
-									<td className="py-4 ps-2 w-50 text-start">
-										Public Key<br />
-										<small>Your wallet's public key (string)</small>
-									</td>
-									<td className="px-5 pt-4">
-										<input type="text" className="form-control" placeholder="Enter Your Wallet's Public Key" value={publicKey} onChange={(e) => setPublicKey(e.target.value)} required />
-									</td>
-								</tr>
-								<tr>
-									<td className="py-4 ps-2 text-start">Name<br />
-										<small>Your NFT Name (string)</small>
-									</td>
-									<td className="px-5 pt-4">
-										<input type="text" className="form-control" placeholder="Enter NFT Name" value={name} onChange={(e) => setName(e.target.value)} required />
-									</td>
-								</tr>
-								<tr>
-									<td className="py-4 ps-2 text-start">
-										Symbol<br />
-										<small>Your NFT Symbol (string)</small>
-									</td>
-									<td className="px-5 pt-4">
-										<input type="text" className="form-control" placeholder="symbol" value={symbol} onChange={(e) => setSymbol(e.target.value)} required />
-									</td>
-								</tr>
-								<tr>
-									<td className="py-4 ps-2 text-start">
-										Description <br />
-										<small>Add a small story to this NFT (string)</small>
-									</td>
-									<td className="px-5 py-3">
-										<textarea className="form-control" placeholder="Enter Description" value={desc} onChange={(e) => setDesc(e.target.value)} required></textarea>
-									</td>
-								</tr>
-								<tr>
-									<td className="py-4 ps-2 text-start">
-										Attributes <br />
-										<small>Attributes associated to this NFT. (Should have 'trait_type' and 'value')</small>
-									</td>
-									<td className="px-5 py-3">
-										<textarea className="form-control" placeholder="Enter Attributes" value={attr} onChange={(e) => setAttr(e.target.value)} required></textarea>
-									</td>
-								</tr>
-								<tr>
-									<td className="py-4 ps-2 text-start">
-										External Url <br />
-										<small>Any url to associate with the NFT</small>
-									</td>
-									<td className="px-5 pt-4">
-										<input type="text" className="form-control" placeholder="Enter Url if Any" value={extUrl} onChange={(e) => setExtUrl(e.target.value)} />
-									</td>
-								</tr>
+										</td>
+									</tr>
+									<tr>
+										<td className="py-4 ps-2 w-50 text-start">
+											Public Key<br />
+											<small>Your wallet's public key (string)(Public Key will be auto detected if it appear blank enter manually from phantom )</small>
+										</td>
+										<td className="px-5 pt-4">
+											<input type="text" className="form-control" placeholder="Enter Your Wallet's Public Key" value={publicKey} onChange={(e) => setPublicKey(e.target.value)} required />
+										</td>
+									</tr>
+									<tr>
+										<td className="py-4 ps-2 text-start">Name<br />
+											<small>Your NFT Name (string)</small>
+										</td>
+										<td className="px-5 pt-4">
+											<input type="text" className="form-control" placeholder="Enter NFT Name" value={name} onChange={(e) => setName(e.target.value)} required />
+										</td>
+									</tr>
+									<tr>
+										<td className="py-4 ps-2 text-start">
+											Symbol<br />
+											<small>Your NFT Symbol (string)</small>
+										</td>
+										<td className="px-5 pt-4">
+											<input type="text" className="form-control" placeholder="symbol" value={symbol} onChange={(e) => setSymbol(e.target.value)} required />
+										</td>
+									</tr>
+									<tr>
+										<td className="py-4 ps-2 text-start">
+											Description <br />
+											<small>Add a small story to this NFT (string)</small>
+										</td>
+										<td className="px-5 py-3">
+											<textarea className="form-control" placeholder="Enter Description" value={desc} onChange={(e) => setDesc(e.target.value)} required></textarea>
+										</td>
+									</tr>
+									<tr>
+										<td className="py-4 ps-2 text-start">
+											Attributes <br />
+											<small>Attributes associated to this NFT. (Should have 'trait_type' and 'value')</small>
+										</td>
+										<td className="px-5 py-3">
+											<textarea className="form-control" placeholder="Enter Attributes" value={attr} onChange={(e) => setAttr(e.target.value)} required></textarea>
+										</td>
+									</tr>
+									<tr>
+										<td className="py-4 ps-2 text-start">
+											External Url <br />
+											<small>Any url to associate with the NFT</small>
+										</td>
+										<td className="px-5 pt-4">
+											<input type="text" className="form-control" placeholder="Enter Url if Any" value={extUrl} onChange={(e) => setExtUrl(e.target.value)} />
+										</td>
+									</tr>
 								</tbody>
 							</table>
 							<div className="p-5 text-center">
@@ -215,12 +215,12 @@ const Create = () => {
 							</div>
 						</div>
 					</form>
-					<div className="text-center">
+					{/* <div className="text-center">
 						This creates one of kind NFTs by setting the <code>max_supply</code> parameter to 0. But you can update it needed, it should be between <i>0-100</i>.
-					</div>
+					</div> */}
 				</div>)}
 
-				<div className="py-5">
+				{/* <div className="py-5">
 					<h2 className="text-center pb-3">Response</h2>
 					<div className="status text-center text-info p-3"><b>{status}</b></div>
 					<textarea
@@ -231,10 +231,10 @@ const Create = () => {
 						cols="30"
 						rows="10"
 					></textarea>
-				</div>
-				<div className="p-3 text-center">
+				</div> */}
+				{/* <div className="p-3 text-center">
 					{dispResponse && (<a href={`https://explorer.solana.com/address/${minted}?cluster=devnet`} target="_blank" className="btn btn-warning m-2 py-2 px-4">View on Explorer</a>)}
-				</div>
+				</div> */}
 			</div>
 		</div>
 	);
